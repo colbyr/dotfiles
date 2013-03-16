06f4027e830d13931883bdbba145c7f0fb69b65e
Colby Rabideau <crabideau5691@gmail.com>
2013-03-15 21:56:01 -0400

    add zshrc to setup script

--- setup.sh
+++ setup.sh
@@ -4,6 +4,7 @@
 ln -s ~/dotfiles/vimrc ~/.vimrc;
 ln -s ~/dotfiles/gitconfig ~/.gitconfig;
 ln -s ~/dotfiles/gitignore_global ~/.gitignore_global;
+ln -s ~/dotfiles/zshrc ~/.zshrc;
 
 # install pathogen
 mkdir -p ~/.vim/autoload ~/.vim/bundle; \
