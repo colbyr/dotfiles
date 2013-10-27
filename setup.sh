53b7fd20bfae2379bb2f7f0eae7d729065ef3b5c
Colby Rabideau <idontknow5691@gmail.com>
2013-10-27 19:07:45 -0400

    better setup

--- setup.sh
+++ setup.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 # sym links
-ln -s ~/dotfiles/vimrc ~/.vimrc;
-ln -s ~/dotfiles/gitconfig ~/.gitconfig;
-ln -s ~/dotfiles/gitignore_global ~/.gitignore_global;
-ln -s ~/dotfiles/zshrc ~/.zshrc;
+ln -fs ~/dotfiles/vimrc ~/.vimrc;
+ln -fs ~/dotfiles/gitconfig ~/.gitconfig;
+ln -fs ~/dotfiles/gitignore_global ~/.gitignore_global;
 
 # install pathogen
 mkdir -p ~/.vim/autoload ~/.vim/bundle; \
@@ -15,3 +14,5 @@ cd ~/.vim/bundle && git clone git://github.com/altercation/vim-colors-solarized.
 
 # install oh my zsh
 curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
+ln -fs ~/dotfiles/zshrc ~/.zshrc;
+source ~/.zshrc
