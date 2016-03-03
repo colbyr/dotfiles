a982bd7a8dc14f7fbbf9640615a41c3446e1e0f1
Colby Rabideau <idontknow5691@gmail.com>
2016-03-02 22:11:56 -0500

    spacemacs?

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # sym links
-ln -fs ~/dotfiles/emacs/emacs.el ~/.emacs;
+ln -fs ~/dotfiles/emacs/spacemacs.el ~/.spacemacs;
 ln -fs ~/dotfiles/vim/vimrc ~/.vimrc;
 ln -fs ~/dotfiles/git/config ~/.gitconfig;
 ln -fs ~/dotfiles/git/ignore_global ~/.gitignore_global;
