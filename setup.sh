feac55f200e2e61183db288cfc7ac2d4b45dd4bb
Colby Rabideau <crabideau@hubspot.com>
2017-02-05 16:58:24 -0500

    plugins and stuff

--- setup.sh
+++ setup.sh
@@ -4,6 +4,8 @@
 ln -fs ~/dotfiles/emacs/spacemacs.el ~/.spacemacs;
 ln -fs ~/dotfiles/hyperterm/hyperterm.js ~/.hyperterm.js;
 ln -fs ~/dotfiles/vim/vimrc ~/.vimrc;
+mkdir -p ~/.config/nvim
+ln -fs ~/dotfiles/vim/vimrc ~/.config/nvim/init.vim
 ln -fs ~/dotfiles/git/config ~/.gitconfig;
 ln -fs ~/dotfiles/git/ignore_global ~/.gitignore_global;
 ln -fs ~/dotfiles/tmux/tmux.conf ~/.tmux.conf;
@@ -16,6 +18,10 @@ touch ~/.zsh_profile
 
 # install Vundle
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+# install plugins in (neo)vim
+vim +PluginInstall +qall
+nvim +PluginInstall +qall
 
 # install oh my zsh
 curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
