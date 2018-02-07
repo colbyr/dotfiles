9f39398620d024eae583df344c01cf8f12bb2ea8
Colby Rabideau <colbyr@spotify.com>
2018-02-06 22:04:03 -0500

    protect git controlled zsh stuff from outside changes

--- setup.sh
+++ setup.sh
@@ -10,16 +10,22 @@ ln -fs ~/dotfiles/tmux/tmux.conf ~/.tmux.conf;
 touch ~/.git_profile
 touch ~/.vim_plugins
 touch ~/.vim_profile
-touch ~/.zsh_profile
+cp ~/dotfiles/zsh/rc_defaults ~/.zshrc
 
 # install Vundle
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 # install plugins in (neo)vim
 vim +PluginInstall +qall
-nvim +PluginInstall +qall
 
 # install oh my zsh
 curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
-ln -fs ~/dotfiles/zshrc ~/.zshrc;
+
+if [ -f ~/.zshrc ]
+then
+  echo "$(cat ~/dotfiles/zsh/rc_defaults)\n\n$(cat ~/.zshrc)" > ~/.zshrc
+else
+  cp ~/dotfiles/zsh/rc_defaults ~/.zshrc
+fi
+
 source ~/.zshrc
