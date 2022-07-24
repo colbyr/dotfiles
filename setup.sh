00e7820d9de47384291ee869cbd7cbb2eb555969
Colby Rabideau <me@colbyr.com>
2022-07-24 11:31:28 -0400

    switch up gitconfig setup

--- setup.sh
+++ setup.sh
@@ -3,36 +3,30 @@
 
 # sym links
 ln -fs ~/dotfiles/vim/vimrc ~/.vimrc;
-ln -fs ~/dotfiles/git/config ~/.gitconfig;
-ln -fs ~/dotfiles/git/ignore_global ~/.gitignore_global;
 ln -fs ~/dotfiles/tmux/tmux.conf ~/.tmux.conf;
-ln -fs ~/dotfiles/Brewfile ~/Brewfile;
 ln -fs ~/dotfiles/finicky.js ~/.finicky.js;
 
 mkdir -p ~/.config/nvim
 ln -fs ~/dotfiles/nvim/init.vim ~/.config/nvim/init.vim;
 
 # install homebrew
-$CURRENT_DIR=$(pwd)
+ln -fs ~/dotfiles/Brewfile ~/Brewfile;
 cd ~
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 brew bundle
-cd $CURRENT_DIR
 
+# git
+ln -fs ~/dotfiles/git/ignore_global ~/.gitignore_global
+cp ~/dotfiles/git/config_defaults ~/.gitconfig
 
 # create profiles for local config
-touch ~/.git_profile
 touch ~/.vim_plugins
 touch ~/.vim_profile
-cp ~/dotfiles/zsh/rc_defaults ~/.zshrc
-
-# install Vundle
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-
-# install plugins in (neo)vim
 vim +PluginInstall +qall
 
 # install oh my zsh
+cp ~/dotfiles/zsh/rc_defaults ~/.zshrc
 curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
 
 if [ -f ~/.zshrc ]
